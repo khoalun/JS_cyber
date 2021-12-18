26eb3df6e4d3ec7856e560b34cf6fed8631b538d
khoalun <buikhoa793@gmail.com>
2021-12-18 16:15:39 +0200

    Function

--- JS-practice/app.js
+++ JS-practice/app.js
@@ -12,9 +12,9 @@ var a = [3, -5, -6, 5, 12, -4, -4];
 
 var sumOdd = 0;
 
-for (var item2 of a) {
-  if (item2 % 2 !== 0) {
-    sumOdd += item2;
+for (var item of a) {
+  if (item % 2 != 0) {
+    sumOdd += item;
   }
 }
 console.log(sumOdd);
@@ -47,16 +47,90 @@ if (firstOddNumber != -1) {
       newArray.push(i);
     }
   }
-  // show all negative number  solution 1,2 
+  // show all negative number  solution 1,2
   // for (i = 0; i < newArray.length; i++) {
   //   index = newArray[i];
 
   //   console.log(" ( " + index + " , " + a[index] + " ) ");
   //  }
   for (var item of newArray) {
-     console.log(" ( " + item + " , " + a[item] + " ) ");
+    console.log(" ( " + item + " , " + a[item] + " ) ");
   }
-
 } else {
   console.log("Array dont have negative number");
 }
+
+// d.Finding largest number in an array
+var b = [5, -5, -6, 5, 12, -4, -4, 12, 12];
+var numberMax = 0;
+
+for (var i = 0; i < b.length; i++) {
+  if (b[i] > b[numberMax]) {
+    numberMax = i;
+  }
+}
+// checking same number
+var newLargestArray = [];
+
+for (var i = numberMax; i < b.length; i++) {
+  if (b[i] === b[numberMax]) {
+    newLargestArray.push(i);
+  }
+}
+// display all number of newarray solution 1, solution 2
+
+for (var item of newLargestArray) {
+  console.log("Largest number of an array: " + b[item] + " located at " + item);
+}
+
+// for (var i = 0; i < newLargestArray; i++) {
+//   var index = newLargestArray[i];
+//   console.log("Largest located at: " + index + " value " + b[index]);
+// }
+
+//Finding largest number by function and sumof3number
+
+function findingLargest(a, b, c) {
+  var number = a;
+  if (b > number) {
+    number = b;
+  }
+  if (c > number) {
+    number = c;
+  }
+  return number;
+}
+
+function sumNumber(a, b, c) {
+  return a + b + c;
+}
+function onClick() {
+  var result = findingLargest(2, 3, 4);
+  var total = sumNumber(3, 5, 6);
+  console.log(result);
+  console.log(total);
+}
+onClick();
+
+function averageScore(math, physic, chemical) {
+  return (math + physic + chemical) / 3;
+}
+
+function validateScore(good, bad, average) {
+  if (averageScore > 8) {
+    return "Good";
+  } else if (6 < averageScore < 8) {
+    return "Normal";
+  } else {
+    return "Bad";
+  }
+}
+
+function onChange() {
+  var result = averageScore(9, 8, 8);
+  var qualify = validateScore(averageScore);
+  console.log(result);
+  console.log(qualify);
+}
+
+onChange();
--- JS-practice/dist/app.dev.js
+++ JS-practice/dist/app.dev.js
@@ -15,10 +15,10 @@ var a = [3, -5, -6, 5, 12, -4, -4];
 var sumOdd = 0;
 
 for (var _i2 = 0, _a2 = a; _i2 < _a2.length; _i2++) {
-  var item2 = _a2[_i2];
+  var item = _a2[_i2];
 
-  if (item2 % 2 !== 0) {
-    sumOdd += item2;
+  if (item % 2 != 0) {
+    sumOdd += item;
   }
 }
 
@@ -49,7 +49,7 @@ if (firstOddNumber != -1) {
     if (a[i] === a[maxOddNumber]) {
       newArray.push(i);
     }
-  } // show all negative number  solution 1,2 
+  } // show all negative number  solution 1,2
   // for (i = 0; i < newArray.length; i++) {
   //   index = newArray[i];
   //   console.log(" ( " + index + " , " + a[index] + " ) ");
@@ -62,4 +62,84 @@ if (firstOddNumber != -1) {
   }
 } else {
   console.log("Array dont have negative number");
+} // d.Finding largest number in an array
+
+
+var b = [5, -5, -6, 5, 12, -4, -4, 12, 12];
+var numberMax = 0;
+
+for (var i = 0; i < b.length; i++) {
+  if (b[i] > b[numberMax]) {
+    numberMax = i;
+  }
+} // checking same number
+
+
+var newLargestArray = [];
+
+for (var i = numberMax; i < b.length; i++) {
+  if (b[i] === b[numberMax]) {
+    newLargestArray.push(i);
+  }
+} // display all number of newarray solution 1, solution 2
+
+
+for (var _i4 = 0, _newLargestArray = newLargestArray; _i4 < _newLargestArray.length; _i4++) {
+  var item = _newLargestArray[_i4];
+  console.log("Largest number of an array: " + b[item] + " located at " + item);
+} // for (var i = 0; i < newLargestArray; i++) {
+//   var index = newLargestArray[i];
+//   console.log("Largest located at: " + index + " value " + b[index]);
+// }
+//Finding largest number by function and sumof3number
+
+
+function findingLargest(a, b, c) {
+  var number = a;
+
+  if (b > number) {
+    number = b;
+  }
+
+  if (c > number) {
+    number = c;
+  }
+
+  return number;
 }
+
+function sumNumber(a, b, c) {
+  return a + b + c;
+}
+
+function onClick() {
+  var result = findingLargest(2, 3, 4);
+  var total = sumNumber(3, 5, 6);
+  console.log(result);
+  console.log(total);
+}
+
+onClick();
+
+function averageScore(math, physic, chemical) {
+  return (math + physic + chemical) / 3;
+}
+
+function validateScore(good, bad, average) {
+  if (averageScore > 8) {
+    return "Good";
+  } else if (6 < averageScore < 8) {
+    return "Normal";
+  } else {
+    return "Bad";
+  }
+}
+
+function onChange() {
+  var result = averageScore(9, 8, 8);
+  var qualify = validateScore(averageScore);
+  console.log(result);
+  console.log(qualify);
+}
+
+onChange();
